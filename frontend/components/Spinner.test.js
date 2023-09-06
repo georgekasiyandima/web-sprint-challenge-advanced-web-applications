d1a43f66cfa9e2d364415746603e4d68900d3b6f
George Kasiyandima <kasiyandimageorge@gmail.com>
2023-09-06 10:41:30 +0200

    tests on spinner passing.

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,20 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from 'react';
+import { render } from '@testing-library/react';
+import Spinner from './Spinner';
+
+describe('Spinner component', () => {
+  it('should not render when "on" prop is false', () => {
+    const { container } = render(<Spinner on={false} />);
+    expect(container.firstChild).toBeNull();
+  });
+
+  it('should render when "on" prop is true', () => {
+    const { getByText } = render(<Spinner on={true} />);
+    const spinnerElement = getByText('Please wait...');
+    expect(spinnerElement).toBeTruthy();
+  });
+});
+
+
